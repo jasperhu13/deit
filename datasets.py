b76bda684e9fb6c30a245273022a4c6c9446c6a8
Jasper Hu <jasperyh@BERKELEY.EDU>
2021-12-08 09:56:55 -0800

    some pytorch

--- datasets.py
+++ datasets.py
@@ -53,7 +53,29 @@ class INatDataset(ImageFolder):
             self.samples.append((path_current, target_current_true))
 
     # __getitem__ and __len__ inherited from ImageFolder
+def has_file_allowed_extension(filename: str, extensions: Tuple[str, ...]) -> bool:
+    """Checks if a file is an allowed extension.
 
+    Args:
+        filename (string): path to a file
+        extensions (tuple of strings): extensions to consider (lowercase)
+
+    Returns:
+        bool: True if the filename ends with one of given extensions
+    """
+    return filename.lower().endswith(extensions)
+
+IMG_EXTENSIONS = ('.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif', '.tiff', '.webp')
+def is_image_file(filename: str) -> bool:
+    """Checks if a file is an allowed image extension.
+
+    Args:
+        filename (string): path to a file
+
+    Returns:
+        bool: True if the filename ends with a known image extension
+    """
+    return has_file_allowed_extension(filename, IMG_EXTENSIONS)
 class ImageNetC(ImageFolder):
     def __init__(
             self,
